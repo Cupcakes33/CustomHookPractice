a97401026fd024b4fbed3dc56f644bbb37a50427
Cupcakes33 <rute930627@gmail.com>
2023-01-04 10:22:42 +0900

    :sparkles: feat: useEventRef

--- src/hooks/useEventRef.js
+++ src/hooks/useEventRef.js
@@ -1,30 +1,30 @@
 import { useRef, useEffect } from "react";
 
 /**
- * 
+ *
  * @param {function} onClick : ref로 참조한 요소에게 클릭했을 시 실행시킬 함수를 정의합니다.
  * @returns {ref} 참조할 대상을 지정합니다.
  */
 
-const useClick = (onClick) => {
+const useEventRef = (fucntion, event) => {
   const ref = useRef();
 
   useEffect(() => {
     const element = ref.current;
-    if (typeof onClick !== "function") {
+    if (typeof fucntion !== "function") {
       return;
     }
 
     if (element) {
-      element.addEventListener("click", onClick);
+      element.addEventListener(event, fucntion);
     }
     return () => {
       if (element) {
-        element.removeEventListener("click", onClick);
+        element.removeEventListener(event, fucntion);
       }
     };
-  }, [onClick]);
+  }, [fucntion, event]);
   return ref;
 };
 
-export default useClick;
+export default useEventRef;
